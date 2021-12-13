8a99d70f17a3cf7e19e36d710930c9ecf3c2012f
AdriánDDS <adriandelgadodelsol@gmail.com>
2021-12-13 17:28:46 +0100

    Feature: added gamemode base class

---
+++ Source/AnimationTests/GameModes/TGTGameModeBase.h
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include "GameFramework/GameModeBase.h"
+#include "TGTGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS(HideCategories = (Cooking, LOD, Game))
+class ANIMATIONTESTS_API ATGTGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+	/*UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 Time;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Type;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FString Value;*/
+};
+
+
